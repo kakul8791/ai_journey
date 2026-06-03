3f2dcd8f89d1348d2a19f1acb2dff69b457f27ab
kakul8791 <rathikakul4@gmail.com>
2026-06-03 21:05:40 +0530

    python logic basic

---
+++ app.py
@@ -0,0 +1,7 @@
+trip_type = input("Enter trip type: ")
+budget = int(input("Enter your budget: "))
+
+if trip_type == "family":
+    print("Recommended Destination: Shimla")
+else:
+    print("Recommended Destination: Goa")
